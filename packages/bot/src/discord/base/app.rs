f896943b0831c7078f631cd917ad3f8946d6d68b
Nathan Miguel <nathan.cm77@gmail.com>
2026-02-03 21:29:57 -0300

    removed reshard system

--- packages/bot/src/discord/base/app.rs
+++ packages/bot/src/discord/base/app.rs
@@ -17,7 +17,6 @@ pub struct App {
     pub http: Arc<Client>,
     pub cache: Arc<InMemoryCache>,
     pub shards: Vec<Shard>,
-    pub intents: Intents,
 }
 
 #[derive(Clone)]
@@ -65,137 +64,24 @@ impl App {
             http,
             cache,
             shards,
-            intents,
         }
     }
 
     pub async fn run(&mut self) {
-        log("Starting bot with reshard system...");
-        
-        // Spawn reshard timer task (60 minutes)
-        let http_clone = self.http.clone();
-        let cache_clone = self.cache.clone();
-        let intents = self.intents;
-        
-        tokio::spawn(async move {
-            App::reshard_timer(http_clone, cache_clone, intents).await;
-        });
-
-        // Start all shards with recovery system
         let mut set = JoinSet::new();
         for shard in self.shards.drain(..) {
-            let shard_id = shard.id();
-            set.spawn(App::shard_runner_with_recovery(
+            set.spawn(App::shard_handle(
                 self.http.clone(),
                 self.cache.clone(),
-                self.intents,
-                shard_id,
+                shard,
             ));
         }
-
-        // Monitor shard tasks
         while let Some(res) = set.join_next().await {
             match res {
-                Ok(_) => log("Shard runner finished."),
-                Err(e) => error(&format!("Shard runner task error: {:?}", e)),
+                Ok(_) => log("Shard finished successfully."),
+                Err(e) => error(&format!("{:?}", e)),
             }
         }
-    }
-
-    /// Timer que executa reshard a cada 60 minutos
-    async fn reshard_timer(http: Arc<Client>, cache: Arc<InMemoryCache>, intents: Intents) {
-        let mut timer = interval(Duration::from_secs(60 * 60 * 6)); // 6 hours
-        timer.tick().await; // Skip first tick
-
-        loop {
-            timer.tick().await;
-            warn("🕰️ Executing scheduled reshard (6 hours interval)...");
-            
-            if let Err(e) = App::perform_reshard(http.clone(), cache.clone(), intents).await {
-                error(&format!("Failed to perform scheduled reshard: {:?}", e));
-            }
-        }
-    }
-
-    /// Executa o processo de reshard
-    async fn perform_reshard(
-        http: Arc<Client>,
-        cache: Arc<InMemoryCache>,
-        intents: Intents,
-    ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-        log("Creating new shards for reshard...");
-        
-        let token = &ENV.BOT_TOKEN;
-        let config = Config::new(token.clone(), intents);
-        let config_callback = |_, builder: ConfigBuilder| builder.build();
-
-        let new_shards = twilight_gateway::create_recommended(&http, config, config_callback)
-            .await?
-            .collect::<Vec<_>>();
-
-        success(&format!("Reshard completed: {} shards created", new_shards.len()));
-
-        // Start new shards
-        for shard in new_shards {
-            let shard_id = shard.id();
-            tokio::spawn(App::shard_runner_with_recovery(
-                http.clone(),
-                cache.clone(),
-                intents,
-                shard_id,
-            ));
-        }
-
-        Ok(())
-    }
-
-    /// Runner de shard com sistema de recuperação automática
-    async fn shard_runner_with_recovery(
-        http: Arc<Client>,
-        cache: Arc<InMemoryCache>,
-        intents: Intents,
-        shard_id: ShardId,
-    ) {
-        loop {
-            log(&format!("Starting shard {} runner...", shard_id.number()));
-            
-            match App::create_and_run_shard(http.clone(), cache.clone(), intents, shard_id).await {
-                Ok(_) => {
-                    log(&format!("Shard {} finished normally", shard_id.number()));
-                    break;
-                }
-                Err(e) => {
-                    error(&format!(
-                        "Shard {} crashed: {:?}. Attempting recovery in 60 seconds...",
-                        shard_id.number(),
-                        e
-                    ));
-                    
-                    // Wait 60 seconds before retry
-                    sleep(Duration::from_secs(60)).await;
-                    log(&format!("Attempting to recreate shard {}...", shard_id.number()));
-                }
-            }
-        }
-    }
-
-    /// Cria e executa uma shard específica
-    async fn create_and_run_shard(
-        http: Arc<Client>,
-        cache: Arc<InMemoryCache>,
-        intents: Intents,
-        shard_id: ShardId,
-    ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-        let token = &ENV.BOT_TOKEN;
-        let config = Config::new(token.clone(), intents);
-
-        let shard = Shard::with_config(shard_id, config);
-        
-        success(&format!("Shard {} created successfully", shard_id.number()));
-        
-        App::shard_handle(http, cache, shard).await;
-        
-        Ok(())
     }
 
     pub async fn shard_handle(
