b70f457b0ed43eec6626570e407e9b4c5261c3df
Nathan Miguel <nathan.cm77@gmail.com>
2026-02-11 19:03:03 -0300

    updated bot automod to unban after a short time

--- packages/bot/src/tools/automod.rs
+++ packages/bot/src/tools/automod.rs
@@ -1,5 +1,9 @@
+use std::mem;
+
 use twilight_http::request::AuditLogReason;
 use twilight_model::channel::Message;
+use chrono::Utc;
+use twilight_util::builder::embed::EmbedBuilder;
 use crate::constants::*;
 use crate::discord::*;
 use crate::functions::*;
@@ -26,6 +30,34 @@ pub async fn mousetrap(ctx: &Context, message: &Message) {
         .await {
         error(&format!("Failed to ban user\n└ {:?}", err));
         return;
+    }
+
+    if let Some(member) = &message.member {
+        if let Some(joined_at) = member.joined_at {
+            let now = Utc::now().timestamp();
+            let joined = joined_at.as_secs();
+            
+            if now - joined > (60 * 60 * 24 * 30) {
+                if let Err(err) = ctx.http.delete_ban(guild_id.clone(), id).await {
+                    error(&format!("Failed to unban {} after being captured by mousetrap\n└ {:?}", &message.author.name, err));
+                } else {
+                    if let Ok(dm_channel) = ctx.http.create_private_channel(id).await {
+                        if let Ok(channel) = dm_channel.model().await {
+                            let dm_content = "Hello! Bangboo has identified that your account sent a series of suspicious messages on the server's channels. As you are a long-time member of the server, your ban has been automatically removed since your account was likely hacked. I recommend that you review your account security!";
+                            let embed = EmbedBuilder::new()
+                                .description(dm_content)
+                                .color(COLORS.warning)
+                                .build();
+                            if let Err(err) = ctx.http.create_message(channel.id)
+                                .embeds(&[embed])
+                                .await {
+                                    error(&format!("Failed to send DM to {} after being captured by mousetrap\n└ {:?}", &message.author.name, err));
+                                }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
